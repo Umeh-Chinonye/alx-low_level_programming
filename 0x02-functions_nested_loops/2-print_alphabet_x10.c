7c9685dba49a3477abf028d97af1d5210b780c3e
Umeh-Chinonye <chinonyeumeh.14@gmail.com>
2022-09-13 15:14:36 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,14 @@
 /**
 * print_alphabet_x10 - entry point
 * Description: program that prints alphabet in lowercase 10 times
-* 
+*
 * Return: 0
 */
 void print_alphabet_x10(void)
 {
 	int alpha;
 	int i = 0;
+
 	while (i <= 9)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
